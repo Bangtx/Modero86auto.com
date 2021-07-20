410cd947d254746dac9b063a54cec09fec887603
Bangtx <bangtran.hha@gmail.com>
2021-07-20 20:43:15 +0700

    .

--- webanhloc/settings.py
+++ webanhloc/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-r!pci2tyeejshv&eqaxtto**purg_53a8ttb22=st3y9vj*=8q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['shielded-wave-80679.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
